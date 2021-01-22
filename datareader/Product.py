68f14118ba526aa497d181728aeb4d11fe8b17b0
Dominik Jašek <jasekdominik@seznam.cz>
2021-01-22 19:58:23 +0100

    remove ugly_json var

--- datareader/Product.py
+++ datareader/Product.py
@@ -5,5 +5,4 @@ class Product:
   def __init__(self, url, id, ugly_json):
     self.url = url
     self.id = id
-    self.json_string = fix_encoding(ugly_json)
-    self.data = json.loads(self.json_string)
+    self.data = json.loads(fix_encoding(ugly_json))
