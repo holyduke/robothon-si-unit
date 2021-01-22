cbb673e0637d971a44e12d6ac8b9d36da8c4d532
Dominik Jašek <jasekdominik@seznam.cz>
2021-01-22 20:18:09 +0100

    fix alza csv parser

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from datareader.csvreader import Csvreader
 
 def main():
     csv_reader = Csvreader("data/")
-    alza_eshop = csv_reader.parse_csv("czc.csv")
+    alza_eshop = csv_reader.parse_csv("alza.csv")
     print(alza_eshop.products[0])
 
 if __name__ == '__main__':
