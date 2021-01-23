1011c9a7de735b62868b7239b344e070765faaa4
Dominik Jašek <jasekdominik@seznam.cz>
2021-01-23 05:31:22 +0100

    fix key name fucking shit

--- datareader/Product.py
+++ datareader/Product.py
@@ -4,17 +4,19 @@ from ftfy import fix_encoding
 
 
 class Product:
-    def __init__(self, url, id, json_str):
+    def __init__(self, url, id, json_obj):
         self.url = url
         self.id = id
-        nfkd_form = unicodedata.normalize('NFKD', json_str)
-        only_ascii = nfkd_form.encode('ASCII', 'ignore')
-        self.data = json.loads(only_ascii)
+        self.data = json_obj
 
     @staticmethod
     def has_valid_data(json_str):
         json_str = json_str.lower()
         json_str = fix_encoding(json_str)        
-        if not 'additionalproperty' in json_str:
+        nfkd_form = unicodedata.normalize('NFKD', json_str)
+        only_ascii = nfkd_form.encode('ASCII', 'ignore')
+        jsonobj = json.loads(only_ascii)
+        if 'name' in jsonobj.keys() and 'additionalproperty' in jsonobj.keys():
+          return jsonobj
+        else:
           return None
-        return json_str if 'name' in json_str else None
