1e85bba7d2370600fc71dd878fb27d9fd4c07c90
Papooch <papoochcz@gmail.com>
2021-01-23 09:58:48 +0100

    updated

--- orchestrator.py
+++ orchestrator.py
@@ -1,5 +1,6 @@
 from searcher.searcher import create_index, add_documents, search
 from sku_matches import *
+import re
 
 def get_props(product):
     return {
@@ -11,20 +12,26 @@ def get_props(product):
 def match_duplicites(eshop, eshop_ix):
     num_of_duplicites = 0
     for product in eshop.products:
-        results = search(eshop_ix, get_props(product), 4)
+        results = search(eshop_ix, get_props(product), 5, use_what=['name'])
         for result in results:
-            if result[0] <= 0.99:
+            if result[0] < .9:
                 continue
             product_again = eshop.products[result[1]]
             if product_again.id == product.id:
                 continue
+            number_set_original = set(re.findall(r"(\w*\d+\w*\d*)", product.data['name']))
+            number_set_again = set(re.findall(r"(\w*\d+\w*\d*)", product_again.data['name']))
+            if (len(number_set_original-number_set_again) != 0 or len(number_set_again-number_set_original) != 0):
+                continue
             if len(product.data['duplicates']) == 0 and len(product_again.data['duplicates']) == 0:
                 num_of_duplicites += 1
             product.data['duplicates'].add(product_again.id)
-            #print('Found duplicate! ' + product.data['name'] + ' == ' + product_again.data['name'])
+            product_again.data['duplicates'].add(product.id)
+            print('Found duplicate! ' + product.data['name'] + ' == ' + product_again.data['name'])
             #print(product.url + ' == ' + product_again.url)
             if len(product.data['duplicates']) >= 4:
                 print('------------------OSHRIRIRIT---------------------')
+                print(product.data['duplicates'])
 
     print('Found ' + str(num_of_duplicites) + ' duplicates in ' + str(len(eshop.products)) + ' items')
     
@@ -35,22 +42,25 @@ def compare_eshops(one, one_ix, two, two_ix):
     for product1 in one.products:
         # search product from eshop 1 in eshop 2
         print('----------\nsearching for: ' + product1.data['name'])
-        result2 = search(two_ix, get_props(product1), 1)[0]
-        if(result2[0] == 0):
+        result2 = search(two_ix, get_props(product1), 1)
+        if(len(result2) == 0):
             print('no match at all')
             continue
         # search the found product form eshop 2 again in eshop 1
-        product2 = two.products[result2[1]]
+        product2 = two.products[result2[0][1]]
         #print('found: ' + product2.data['name'])
-        result1 = search(one_ix, get_props(product2), 1)[0]
-        product1_again = one.products[result1[1]]
+        result1 = search(one_ix, get_props(product2), 1)
+        if(len(result1) == 0):
+            print('no reverse match')
+            continue
+        product1_again = one.products[result1[0][1]]
         #print('which in turn found: ' + product1_again.data['name'])
         if product1.id in [product1_again.id, *product1_again.data['duplicates']]:
             print('yay, found match!')
             print(product1.data['name'] + " == " + product2.data['name'])
             num_of_matches += 1
-            print('conf. value 1: ' + str(round(result1[0], 2)))
-            print('conf. value 2: ' + str(round(result2[0], 2)))
+            print('conf. value 1: ' + str(round(result1[0][0], 2)))
+            print('conf. value 2: ' + str(round(result2[0][0], 2)))
         else:
             print('match not found')
     print('Matched: ' + str(num_of_matches) + ' of ' + str(len(one.products)))
--- searcher/searcher.py
+++ searcher/searcher.py
@@ -24,7 +24,7 @@ def add_documents(ix, docs):
         name=" ".join(doc.data['key_name']) if isinstance(doc.data['key_name'], set) else doc.data['key_name']
         numbers=" ".join(doc.data['key_numbers']) if isinstance(doc.data['key_numbers'], set) else doc.data['key_numbers']
         strings=" ".join(doc.data['key_words']) if isinstance(doc.data['key_words'], set) else doc.data['key_words']
-        if idx > 0 and idx % 10 == 0:
+        if idx > 0 and idx % 100 == 0:
             print('added ' + str(idx) + ' documents...')
         writer.add_document(
             name=name,
@@ -35,19 +35,22 @@ def add_documents(ix, docs):
     writer.commit()
     print('added ' + str(len(docs)) + ' documents in total.')
 
-def search(ix, tags, max_results=3):      
+def search(ix, tags, max_results=3, use_what=['name']):      
 
     matches = []
     with ix.searcher() as searcher:
-        name_terms = [Term('name', n) for n in tags["name"]]
-        #string_terms = [Term('strings', n) for n in tags["strings"]]
-        #number_terms = [Term('numbers', n) for n in tags["numbers"]]
-        all_terms = [*name_terms]#, *number_terms]#, *string_terms]
+        all_terms = []
+        if 'name' in use_what:
+            all_terms.extend([Term('name', n) for n in tags["name"]])
+        if 'strings' in use_what:
+            all_terms.extend([Term('strings', n) for n in tags["strings"]])
+        if 'numbers' in use_what:
+            all_terms.extend([Term('numbers', n) for n in tags["numbers"]])
 
         q = Or(all_terms)
         results = searcher.search(q, terms=True, limit=max_results)
         if(len(results) == 0):
-            return [0, 0, []]
+            return []
         #print(results[0].matched_terms())
         #print([[x.score/len(all_terms), x] for x in results])
         for i in range(min(max_results, len(results))):
